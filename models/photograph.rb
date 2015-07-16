a6230b0cd9743b36ae349d2367963a2c96cd84c4
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-16 14:00:42 -0500

    Adds Album relationship
    Adds top photo method

--- models/photograph.rb
+++ models/photograph.rb
@@ -1,8 +1,13 @@
 class Photograph < ActiveRecord::Base
   belongs_to :photographer
+  has_and_belongs_to_many :albums
   
   validates :name, presence: true
   validates :photographer, presence: true
 
+
+  def top_photo?
+    albums.length > 2
+  end
   
 end
